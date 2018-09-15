f193c7d696c35cc5c5dd6d7d015b28be4e206c4d
Manny <32347232+Mlpichardo1@users.noreply.github.com>
2018-09-15 11:14:51 -0400

    Destructering

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,6 +3,14 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+    ninjas : [
+      { name: 'Ryu', age: 30, belt: black, id: 1},
+      { name: 'Ken', age: 28, belt: black, id: 2},
+      { name: 'Shino', age: 33, belt: red, id: 3}
+    ]
+  }
+
   render() {
     return (
       <div className="App">
--- my-app/src/Ninjas.js
+++ my-app/src/Ninjas.js
@@ -3,13 +3,20 @@ import React, { Component } from 'react';
 class Ninjas extends Component{
     render(){
         // console.log(this.props);
-        const { name, age, belt } = this.props; // example of destructering
+        const { ninjas } = this.props; // example of destructering
+        const ninjaList = ninjas.map(ninja => {
+            return (
+                <div className="ninja" key={ninja.id}>
+                    <div>Name: { ninja.name }</div>
+                    <div>Age: { ninja.age }</div>
+                    <div>Belt: { ninja.belt }</div>
+                </div>
+            )
+        })
         return(
-            <div className="ninja">
-                <div>Name: { this.props.name }</div>
-                <div>Age: { age }</div>
-                <div>Belt: { belt }</div>
-            </div>
+            <div className="ninja-list">
+               { ninjaList }
+            </div> 
         )
     }
 }
